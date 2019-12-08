23fea2dd989d5643e1f6cff2103094b20f6b7b3c
alibalouchi <30617639+alibalouchi@users.noreply.github.com>
2019-12-08 10:47:00 +0330

    create tic-tac-toe app with vue

---
+++ src/components/Board.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Board">
+        <Square v-on:changeValue="handleClick($event)" id="0" />
+    </div>
+</template>
+
+<script>
+import { Square } from './Square'
+export default {
+    el: "Board",
+    components: {
+        Square
+    },
+    handleClick(){
+        window.alert("salam")
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="square" @click="$emit('changeValue', this.props.id)">salam</div>    
+</template>
+
+<script>
+export default {
+    el: "square",
+    name: "Square",
+    props: {
+        value: Number,
+        xTurn: Boolean
+    },
+    data(){
+        return {
+            // value: this.props.xTurn == true ? "X" : "O",
+        }
+    }
+}
+</script>
+
+<style>
+    .square {
+        width: 100px;
+        height: 100px;
+        border: 1px solid black;
+    }
+</style>
